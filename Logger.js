c97ea058f9f151b2ea84db029fc80e52e7d6a7fa
Chase McManning <cmcmanning@gmail.com>
2013-09-06 12:08:09 -0400

    Implemented server pushing of error logs (Sybolt#81)

--- Logger.js
+++ Logger.js
@@ -14,8 +14,30 @@ fro.log = {
 	 * @param target Indicates where our logs will be printed to.
 	 *     Should be a jQuery selector
 	 */
-	initialise : function(target) {
-		this.target = target;
+	initialise : function(options) {
+	
+		this.logToConsole = options.logToConsole;
+	
+		if ('logServer' in options) {
+			this.logServer = options.logServer;
+			this.upstreamBuffer = '';
+			
+			this.interval = fro.timers.addInterval(this, function() {
+			
+				if (this.upstreamBuffer.length > 0) {
+					// We don't care what comes back, just send it up
+					$.ajax({
+						url: this.logServer,
+						type: 'POST',
+						data: { logs : this.upstreamBuffer }
+					});
+					
+					this.upstreamBuffer = '';
+				}
+				
+			}, 5000, false);
+		}
+		
 	},
 
 	debug : function(message) {
@@ -51,20 +73,12 @@ fro.log = {
 		var msg = '[' + this.getLevelString(level) + '] ' + message.toString();
 		
 		// If we send important logs to the server, queue them up
-		if (level >= this.WARNING && this.writeToServer) { // @todo implement server push
-			this.queue.push(msg);
+		if (level >= this.WARNING && this.logServer) {
+			this.upstreamBuffer += msg.toString() + '\n';
 		}
 		
-		if (this.target) {
-			this.target.append('<span class="log-level-' + level + '">' + msg + '</span>');
-			this.target.scrollTop(this.target[0].scrollHeight);
-		
-		} else { // Fall back to console
-			console.log(msg);
-		}
-		
-		// @todo proper object printing
-		if (typeof message != 'string')
+		if (this.logToConsole) {
 			console.log(message);
+		}
 	}
 }
