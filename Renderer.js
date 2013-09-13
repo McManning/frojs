ada26c98a64b3b0f1b455094b2a4b95656432953
Chase McManning <cmcmanning@gmail.com>
2013-09-13 17:52:58 -0400

    Changed texture mag filter to gl.LINEAR, instead of NEAREST to maintain hard pixel accuracy while zooming

--- Renderer.js
+++ Renderer.js
@@ -118,7 +118,7 @@ fro.renderer = {
 			//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 			//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 			//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);  
 
 			// Supporting non power of two textures
