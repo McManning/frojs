d8481f1eb60d36dacbe7d1fe75f58786eb934e5f
Chase McManning <cmcmanning@gmail.com>
2015-06-20 13:45:32 -0400

    Setting up Grunt for a RequireJS build

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,49 +3,64 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                src: ['src/**/*.js'],
-                dest: 'dist/<%= pkg.name %>.js'
-            }
-        },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-                preserveComments: false,
-                sourceMap: true,
-                sourceMapName: "dist/<%= pkg.name %>.map"
-            },
-            dist: {
-                files: {
-                    'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
-                }
-            }
-        }
         jshint: {
             files: ['Gruntfile.js', 'src/**/*.js'],
             options: {
                 globals: {
-                    jQuery: true,
                     console: true,
                     module: true
                 }
             }
         },
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: './src',
+                    mainConfigFile: 'src/config.js',
+                    // Uncomment these lines if we want to use almond instead
+                    // and probably manually set a wrapper to something that makes
+                    // more sense.
+                    //name: '../external/almond',
+                    //include: 'fro',
+                    wrap: true,
+                    name: 'fro',
+                    optimize: 'none', // going to do this as a separate task
+                    out: 'dist/fro.js',
+                    exclude: [
+                        'jquery'
+                    ]
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+                preserveComments: false,
+                sourceMap: true,
+                sourceMapName: "dist/fro.map"
+            },
+            dist: {
+                files: {
+                    'dist/fro.min.js': ['dist/fro.js']
+                }
+            }
+        },
         watch: {
-            files: ['Gruntfile.js', 'src/**/*.js'],
-            tasks: ['jshint']
+            files: ['Gruntfile.js', 'require-config.js', 'src/**/*.js'],
+            tasks: ['dev']
         }
     });
+    
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
 
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+    // Faster build task for dev builds on watch
+    grunt.registerTask('dev', ['jshint', 'requirejs']);
+
+    // Full build task
+    grunt.registerTask('default', ['jshint', 'requirejs', 'uglify']);
 
 };
