1bf61abe70c49371398635e22b4e5a89893583dc
Chase McManning <cmcmanning@gmail.com>
2015-06-20 14:53:27 -0400

    Modularizing and linting

--- src/EventHooks.js
+++ src/EventHooks.js
@@ -16,184 +16,150 @@
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+define([], function() {
 
-"use strict";
+    /**
+     * A basic extension to bind event listeners to objects
+     */
+    return {
+        bind: function(evt, obj, fn) {
 
-/**
- * A basic extension to bind event listeners to objects
- */
-var EventHooks = {
-
-    bind: function( evt, obj, fn ) {
-
-        if (obj == null && fn == null) { // (evt)
-            return this;
-        } else if (typeof obj == 'function') { // (evt, fn)
-            fn = obj;
-            obj = this;
-        } // otherwise, (evt, obj, fn)
-        
-        var namespaces = '';
-        
-        // Split multiple event bindings into multiple triggers
-        var events = evt.split(',');
-        
-        for (var i in events) {
+            if (obj === null && fn === null) { // (evt)
+                return this;
+            } else if (typeof obj === 'function') { // (evt, fn)
+                fn = obj;
+                obj = this;
+            } // otherwise, (evt, obj, fn)
             
-            // Clean up each bind
-            evt = events[i].trim();
-
-            // Check if the bind is namespaced
-            if (evt.indexOf('.') >= 0) {
-                namespaces = evt.split('.');
-                evt = namespaces.shift();
-                namespaces.sort();
-                namespaces = namespaces.join('.');
-            }
+            var namespaces = '';
             
-            if (this._events == undefined) {
-                this._events = {};
-                this._events[evt] = new Array();
+            // Split multiple event bindings into multiple triggers
+            var events = evt.split(',');
+            
+            for (var i in events) {
                 
-            } else if (!(evt in this._events)) {
-                this._events[evt] = new Array();
-            }
-            
-            this._events[evt].push({
-                callback: fn,
-                obj: obj,
-                namespaces: namespaces,
-            });
-        }
-        
-        return this;
-    },
-    
-    unbind: function(evt, fn) {
-        
-        throw 'Needs refactoring';
-        
-        // @todo also allow (obj), (evt, obj) calls
-        if (fn == null) { 
-            if (typeof evt == 'function') { // (fn)
-                fn = evt;
-                evt = undefined;
-            } else if (evt == null) { // ()
-                evt = undefined;
-                fn = undefined;
-            } else { // (evt)
-                fn = undefined;
-            }
-        }
-        
-        if (evt != undefined) {
-            
-            if (evt in this._events) {
-                if (fn != undefined) {
-                    // Remove a specific function reference for this event
+                // Clean up each bind
+                evt = events[i].trim();
 
-                    var evts = this._events[evt];
+                // Check if the bind is namespaced
+                if (evt.indexOf('.') >= 0) {
+                    namespaces = evt.split('.');
+                    evt = namespaces.shift();
+                    namespaces.sort();
+                    namespaces = namespaces.join('.');
+                }
+                
+                if (typeof this._events === 'undefined') {
+                    this._events = {};
+                    this._events[evt] = [];
                     
-                    var len = evts.length;
-                    while (len--) {
-                        if (evts[len] == fn) {
-                            evts.splice(len, 1);
-                        }
-                    }
-                    
-                } else { 
-                    // Remove all binds for this event
-                    
-                    delete this._events[evt];
+                } else if (!(evt in this._events)) {
+                    this._events[evt] = [];
+                }
+                
+                this._events[evt].push({
+                    callback: fn,
+                    obj: obj,
+                    namespaces: namespaces,
+                });
+            }
+            
+            return this;
+        },
+        
+        unbind: function(evt, fn) {
+            
+            // TODO: Refactor all of this actual logic, it's outdated.
+
+            var len;
+
+            // @todo also allow (obj), (evt, obj) calls
+            if (fn === null) { 
+                if (typeof evt === 'function') { // (fn)
+                    fn = evt;
+                    evt = undefined;
+                } else if (evt === null) { // ()
+                    evt = undefined;
+                    fn = undefined;
+                } else { // (evt)
+                    fn = undefined;
                 }
             }
             
-        } else {
-            
-            if (fn != undefined) {
-                // Remove a specific function from all events
-                for (e in this._events) {
-                    var len = this._events[e].length;
-                    while (len--) {
-                        if (this._events[e][len] == fn) {
-                            this._events[e].splice(len, 1);
+            if (typeof evt !== 'undefined') {
+                
+                if (evt in this._events) {
+                    if (typeof fn !== 'undefined') {
+                        // Remove a specific function reference for this event
+
+                        len = this._events[evt].length;
+                        while (len--) {
+                            if (this._events[evt][len] == fn) {
+                                this._events[evt].splice(len, 1);
+                            }
                         }
+                        
+                    } else { 
+                        // Remove all binds for this event
+                        delete this._events[evt];
                     }
                 }
                 
             } else {
-                // Remove all binds
-                this._events = {};
-            }
-        }
-        
-        return this;
-    },
-    
-    fire: function(evt, data) {
-    
-        var namespaces_re = null;
-        
-        if (evt.indexOf('.') >= 0) {
-            var namespaces = evt.split('.');
-            evt = namespaces.shift();
-            
-            if (namespaces.length > 0) {
-                namespaces.sort();
-                namespaces_re = new RegExp( "(^|\\.)"
-                                + namespaces.join("\\.(?:.*\\.|)") 
-                                + "(\\.|$)" );
-            }
-        }
-        
-        if (this._events != undefined) {
-            if (evt in this._events) { 
-                for (var e in this._events[evt]) {
-                    
-                    var fn = this._events[evt][e];
-                    
-                    if (!namespaces_re || namespaces_re.test( fn.namespaces )) {
-                        try {
-                            fn.callback.apply( fn.obj, [data] );
-                        } catch (e) {
-                            fro.log.error('Exception during event ' + evt + ': ' + e.stack);
+                
+                if (typeof fn !== 'undefined') {
+                    // Remove a specific function from all events
+                    for (var e in this._events) {
+                        len = this._events[e].length;
+                        while (len--) {
+                            if (this._events[e][len] == fn) {
+                                this._events[e].splice(len, 1);
+                            }
                         }
+                    }
+                    
+                } else {
+                    // Remove all binds
+                    this._events = {};
+                }
+            }
+            
+            return this;
+        },
+        
+        fire: function(evt, data) {
+        
+            var namespaces_re = null;
+            
+            if (~evt.indexOf('.')) {
+                var namespaces = evt.split('.');
+                evt = namespaces.shift();
+                
+                if (namespaces.length > 0) {
+                    namespaces.sort();
+                    namespaces_re = new RegExp( "(^|\\.)" +
+                                    namespaces.join("\\.(?:.*\\.|)") +
+                                    "(\\.|$)" );
+                }
+            }
+            
+            if (typeof this._events === 'undefined' || !(evt in this._events)) {
+                return;
+            }
+
+            for (var e in this._events[evt]) {
+                var fn = this._events[evt][e];
+                
+                if (!namespaces_re || namespaces_re.test( fn.namespaces )) {
+                    try {
+                        fn.callback.apply( fn.obj, [data] );
+                    } catch (exception) {
+                        throw new Error('Exception during event ' + evt + ': ' + exception.stack);
                     }
                 }
             }
         }
         
-    }
-    
-};
+    };
 
-
-function eventhooks_unit_tests() {
-    
-    EventHooks.bind('testevt', function() {
-        console.log('testevt fired');
-    })
-    .bind('testevt.my_namespace', function() {
-        console.log('testevt.my_namespace fired');
-    })
-    .bind('testevt.otherns.my_namespace', function() {
-        console.log('testevt.otherns.my_namespace fired');
-    })
-    .bind('testevt.other.blah', function() {
-        console.log('testevt.other.blah fired');
-    })
-    .bind('testevt.other', function() {
-        console.log('testevt.other fired');
-    })
-    .bind('blah', function() {
-        console.log('blah fired');
-    });
-    
-    EventHooks.fire('testevt.my_namespace');
-    console.log('------');
-    EventHooks.fire('testevt');
-    console.log('------');
-    EventHooks.fire('blah');
-    console.log('------');
-    EventHooks.fire('other');
-}
+});
