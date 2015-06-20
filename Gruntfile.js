65b88069e11d2d0558a2f6cd7e9a5854f507e307
Chase McManning <cmcmanning@gmail.com>
2015-06-20 00:21:24 -0400

    Cleaning up main directory

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+    "use strict";
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+                preserveComments: false,
+                sourceMap: true,
+                sourceMapName: "dist/<%= pkg.name %>.map"
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
+                }
+            }
+        }
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js'],
+            options: {
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true
+                }
+            }
+        },
+        watch: {
+            files: ['Gruntfile.js', 'src/**/*.js'],
+            tasks: ['jshint']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+};
