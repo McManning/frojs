e7e0bf30bb81c4a8b1fdc5a5cc01b7c2b9081b45
Chase McManning <cmcmanning@gmail.com>
2015-07-03 12:25:25 -0400

    Adding parent/child entity relationships

--- src/entity/Entity.js
+++ src/entity/Entity.js
@@ -35,24 +35,88 @@ define([
         this.offset = vec3.create();
         this.translation = vec3.create();
         this.context = context;
+
+        this.children = [];
+        this.parent = null;
     }
 
     Entity.prototype.destroy = function() {
 
         // Fire a destroy event to any listeners 
         this.fire('destroy');
+
+        // If we have a parent, detach ourselves from it
+        if (this.parent) {
+            this.parent.removeChild(this);
+        }
+
+        // Destroy children as well
+        if (this.children) {
+            for (var i = 0; i < this.children.length; i++) {
+                this.children[i].setParent(null);
+                this.children[i].destroy();
+            }
+        }
         
         // Nuke the entity itself
         this.context.world.remove(this);
     };
 
-    Entity.prototype.getPosition = function() {
-        return this.position;
+    /**
+     * Add a new entity as a child to this. Children entities
+     * have their position always relative to the parent. Whereas
+     * translation is their actual world position. 
+     */
+    Entity.prototype.addChild = function(entity) {
+        this.children.push(entity);
+        entity.setParent(this);
+        this.fire('add', entity);
+    };
+
+    Entity.prototype.removeChild = function(entity) {
+        var index = this.children.indexOf(entity);
+        if (~index) {
+            this.children.splice(index, 1);
+            entity.setParent(null);
+            this.fire('remove', entity);
+        }
+    };
+
+    Entity.prototype.setParent = function(entity) {
+        this.parent = entity;
+        this.fire('parent', entity);
     };
 
     /**
-     * Set the entity's position. Accepts either an (x,y) pair
-     * or an (x,y,z) to also specify the z-order.
+     * Returns a new vec3 representing our position in local space.
+     * If this entity does not have a parent, this is equivalent to
+     * our position in world space. Otherwise, it is relative to 
+     * the parent's position.
+     */
+    Entity.prototype.getPosition = function() {
+
+        return vec3.create(this.position);
+    };
+
+    /** 
+     * Returns a new vec3 representing our position in world space.
+     *
+     * @return {vec3} 
+     */
+    Entity.prototype.getWorldPosition = function() {
+
+        var pos = this.getPosition();
+        if (this.parent) {
+            vec3.add(pos, this.parent.getWorldPosition());
+        }
+
+        return pos;
+    };
+
+    /**
+     * Set the entity's position in local space. 
+     * Accepts either an (x,y) pair or an (x,y,z) 
+     * to also specify the z-order.
      *
      * @param {vec3} position
      */
@@ -73,8 +137,8 @@ define([
     };
 
     /**
-     * Set the offset coordinates that our image renders from 
-     * our base position. 
+     * Set the offset coordinates (in pixels) that our
+     *  image renders from our position in local space. 
      *
      * @param {vec3} offset
      */
@@ -105,16 +169,26 @@ define([
 
     Entity.prototype.updateTranslation = function() {
         
-        // update render translation appropriately
-        this.translation[0] = this.position[0] + this.offset[0];
-        this.translation[1] = this.position[1] + this.position[2] + this.offset[1]; // y + z for offsetting according to z-height
+        // update world render translation appropriately
+        var pos = this.getWorldPosition();
+        
+        vec3.add(pos, this.offset);
+
+        // If translation changes, update translation of children as well
+        if (!vec3.equals(pos, this.translation)) {
+            vec3.set(pos, this.translation);
+
+            for (var i = 0; i < this.children.length; i++) {
+                this.children[i].updateTranslation();
+            }
+        }
     };
 
     /**
      * @return {boolean} 
      */
     Entity.prototype.collides = function(r) {
-        return false && r; // Tiny hack for jshint.
+        // jshint unused: false
     };
 
     return Entity;
