2cf90c9cf98ab01243c03b732624281b26bf945b
Chase McManning <cmcmanning@gmail.com>
2015-07-14 21:20:07 -0400

    Condensing code

--- src/fro.js
+++ src/fro.js
@@ -18,53 +18,38 @@
  */
 
 define(function(require) {
-    // Resources exposed to the global context
-
-    // Public modules
-    var World = require('World'),
-        Timer = require('Timer'),
-        utils = require('Utility');
-
-        // Note: 'utils' naming is intentional since 
-        // it's an object literal, not a constructor.
-
-    // Entity types
-    var Entity = require('entity/Entity'),
-        Actor = require('entity/Actor'),
-        Prop = require('entity/Prop'),
-        RemotePlayer = require('entity/RemotePlayer'),
-        Sound = require('entity/Sound');
-
-    // Resource types
-    var Animation = require('resource/Animation'),
-        FontImage = require('resource/FontImage'),
-        Image = require('resource/Image'),
-        Json = require('resource/Json'),
-        Shader = require('resource/Shader'),
-        SoundResource = require('resource/Sound');
 
     // Expose modules
     return {
-        World: World,
-        Timer: Timer,
-        utils: utils,
+        VERSION: '0.1.0',
+
+        // Expose object constructors
+        World: require('World'),
+        Timer: require('Timer'),
+        utils: require('Utility'),
+        // Note: utils naming convention is intentional since
+        // it's an object literal, not a constructor.
+
+        // Expose entity types
         entities: {
-            Entity: Entity,
-            Actor: Actor,
-            Prop: Prop,
-            RemotePlayer: RemotePlayer,
-            Sound: Sound
+            Entity: require('entity/Entity'),
+            Actor: require('entity/Actor'),
+            Prop: require('entity/Prop'),
+            RemotePlayer: require('entity/RemotePlayer'),
+            Sound: require('entity/Sound')
         },
+
+        // Expose resource types
         resources: {
-            Animation: Animation,
-            FontImage: FontImage,
-            Image: Image,
-            Json: Json,
-            Shader: Shader,
-            Sound: SoundResource
+            Animation: require('resource/Animation'),
+            FontImage: require('resource/FontImage'),
+            Image: require('resource/Image'),
+            Json: require('resource/Json'),
+            Shader: require('resource/Shader'),
+            Sound: require('resource/Sound')
         },
-        plugins: {
-            // Added to externally
-        }
+
+        // Expose an empty container for external plugins
+        plugins: {}
     };
 });
