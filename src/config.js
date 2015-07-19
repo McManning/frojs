2a58c7d03e71ca20404cdcaa830f6b3e6d814777
Chase McManning <cmcmanning@gmail.com>
2015-07-19 17:40:12 -0400

    Adding shortcuts Actor.setState & Actor.getState

--- src/config.js
+++ src/config.js
@@ -7,5 +7,9 @@ requirejs.config({
     paths: {
         text: 'vendor/text',
         glMatrix: 'vendor/glMatrix'
-    }
+        //browser: '../node_modules/debug/browser',
+        //debug: '../node_modules/debug/debug',
+        //ms: '../node_modules/debug/node_modules/ms/index'
+    },
+    cjsTranslate: true,
 });
