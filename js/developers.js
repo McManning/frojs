5e6002f3a8c8d7aab4c8fa6626fe6b3fc3323eed
Chase McManning <cmcmanning@gmail.com>
2015-08-16 18:49:46 -0400

    Adding a developers page for detailed usage

---
+++ js/developers.js
@@ -0,0 +1,29 @@
+
+/*
+requirejs.config({
+    paths: {
+        'fro': 'vendor/fro.min',
+        //'Nametag': 'plugins/Nametag'
+        //'plugins': '../../src/plugins',
+        //'frochat': '../../../frochat/src/frochat',
+        //'emojify': '../../../frochat/src/vendor/emojify.min'
+    },
+    //baseUrl: '../../src',
+    //plugins: '../../examples/test'
+    //urlArgs: 'bust=' + Date.now()
+});
+
+require([
+    'fro'
+], function(fro) {
+
+});
+
+*/
+
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+
+    
+});
