88203c2710eaf8247e9af804a4b53006e301fae0
Chase McManning <cmcmanning@gmail.com>
2015-08-16 20:25:38 -0400

    Doing dumb icon stuff on scroll

--- js/developers.js
+++ js/developers.js
@@ -25,5 +25,23 @@ require([
 $(document).ready(function(){
     $('.scrollspy').scrollSpy();
 
-    
+    var scrollTimer = null;
+    $(window).scroll(function() {
+        if (scrollTimer) {
+            clearTimeout(scrollTimer);
+        }
+        scrollTimer = setTimeout(onScroll, 50);
+    });
+
+    function onScroll() {
+        scrollTimer = null;
+
+        var top = $(window).scrollTop();
+        if (top > 20) {
+            $('#frojs-logo').addClass('large');
+        } else {
+            $('#frojs-logo').removeClass('large');
+        }
+    }
+
 });
